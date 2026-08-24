09274de3c04ced8fea6681810fc3a16494e1dd67
zec-ux <lukazekanovic4@gmail.com>
2026-08-24 18:23:22 +0200

    Pages and browser router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,32 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
+import Order from './pages/Order';
+import Profile from './pages/Profile';
+import Admin from './pages/Admin';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
 function App() {
   return (
     <>
-      <h1 className='text-3xl text-teal-600'>VoltFlow</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/product/:id' element={<Product />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/checkout' element={<Checkout />} />
+          <Route path='/order/:id' element={<Order />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/admin' element={<Admin />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
